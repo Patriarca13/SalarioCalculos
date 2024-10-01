15d1c6a4b74f1e77e46bac8a2b843e2d5f730b71
joaoPatriarca <joaomarcospatriarca@outlook.com>
2024-09-30 21:12:52 -0300

    Adicionar arquivos de projeto.

---
+++ SalarioCalculos/Contas.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalarioCalculos
+{
+    internal class Contas
+    {
+        public double SalarioBase { get; set; }
+        public int StarPlus { get; set; }
+
+        public int Fx { get; set; }
+        public int X30 { get; set; }
+        public double Imposto { get; set; }
+        public double Porcentagem { get; set; }
+
+
+
+        public int BonusStarplus()
+        {
+            return StarPlus * 25;
+        }
+        public int bonusFx()
+        {
+            return Fx * 35;
+        }
+        public double BonusX30()
+        {
+            return X30 * 12.50;
+        }
+        public double ValorTotal()
+        {
+            return BonusStarplus() + bonusFx() + BonusX30();
+        }
+
+        public double SalarioTotal()
+        {
+            return ValorTotal() + SalarioBase;
+        }
+        public double SalarioPercentual()
+        {
+            double salarioTotal = SalarioTotal();
+            double bonusPercentual = salarioTotal * (Imposto / 100);
+            return salarioTotal - bonusPercentual;
+        }
+        public double SalarioComPercentual()
+        {
+            double salarioTotal = SalarioPercentual();
+            double bonusPercentual = salarioTotal * (Porcentagem / 100);
+            return salarioTotal + bonusPercentual;
+        }
+    }
+}
---
+++ SalarioCalculos/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Globalization;
+using SalarioCalculos;
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Contas contas = new Contas();
+
+        Console.Write("Qual é o seu salario base : ");
+        contas.SalarioBase = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.Write("Qual o desconto do inss: ");
+        contas.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.Write("Deseja adicionar um bônus percentual? (s/n)");
+        char resp = char.Parse(Console.ReadLine());
+
+        if (resp == 's' || resp == 'S')
+        {
+            Console.WriteLine("Qual a porcentagem do seu bônus: ");
+            contas.Porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        }
+        else if (resp == 'n' || resp == 'N')
+        {
+            Console.WriteLine("Nenhum bônus percentual foi adicionado.");
+        }
+
+        Console.WriteLine("-----------------------------------------------------------------------------------------------");
+        Console.WriteLine("Quantas linhas  foram montadas esse mes: ");
+        contas.StarPlus = int.Parse(Console.ReadLine());
+        contas.Fx = int.Parse(Console.ReadLine());
+        contas.X30 = int.Parse(Console.ReadLine());
+        Console.WriteLine("-----------------------------------------------------------------------------------------------");
+        Console.WriteLine("O valor total em bonus foi de R$: " + contas.ValorTotal() + " e o salario total foi R$ : " +
+                            contas.SalarioTotal().ToString("F2"));
+        Console.WriteLine("O seu salario com o desconto do inss foi: " + contas.SalarioPercentual().ToString("F2"));
+        Console.WriteLine("-----------------------------------------------------------------------------------------------");
+        Console.WriteLine();
+        Console.WriteLine("O seu salario com bônus foi  " + contas.SalarioComPercentual().ToString("F2"));
+        Console.WriteLine("-----------------------------------------------------------------------------------------------");
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
